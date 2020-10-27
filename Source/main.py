5126a4d58a92a2cbb33a937333f1eaa5b493be2f
RagonR <rapolasrusakevicius@gmail.com>
2020-10-27 15:52:37 +0200

    1.8 refactor element names + changed the language of the comments

--- Source/main.py
+++ Source/main.py
@@ -1,93 +1,94 @@
 class TicTacToe:
-
     def __init__(self):
         self._squares = {}
         self._winningCombos = (
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], #Is kairės į desine
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], #Is virsaus į apačią
-        [0, 4, 8], [2, 4, 6]) #Įstrižai
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontally
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertically
+            [0, 4, 8], [2, 4, 6])  # Diagonally
 
-    def createBoard(self):
+    def create_board(self):
         for i in range(9):
             self._squares[i] = "."
         print(self._squares)
 
-    def showBoard(self):
+    def show_board(self):
         print()
         print(self._squares[0], self._squares[1], self._squares[2])
         print(self._squares[3], self._squares[4], self._squares[5])
         print(self._squares[6], self._squares[7], self._squares[8])
 
-    def getAvailableMoves(self):
+    def get_available_moves(self):
         self._availableMoves = []
-        for i in range(9) :
+        for i in range(9):
             if self._squares[i] == ".":
                 self._availableMoves.append(i)
         return self._availableMoves
 
-    def makeMove(self, position, player):
+    def make_move(self, position, player):
         self._squares[position] = player
-        self.showBoard()
+        self.show_board()
 
     def complete(self):
         if "." not in self._squares.values():
             return True
-        if self.getWinner() != None:
+        if self.get_winner() is not None:
             return True
         return False
 
-    def getWinner(self):
+    def get_winner(self):
         for player in ("x", "o"):
             for combos in self._winningCombos:
-                if self._squares[combos[0]] == player and self._squares[combos[1]] == player and self._squares[combos[2]] == player:
+                if self._squares[combos[0]] == player and self._squares[combos[1]] == player and self._squares[
+                    combos[2]] == player:
                     return player
         if "." not in self._squares.values():
             return "tie"
         return None
 
-    def getEnemyPlayer(self, player):
-        if player == "x" :
+    def get_enemy_player(self, player):
+        if player == "x":
             return "o"
         return "x"
 
-
-    def minimax(self, player, depth = 0):
+    def mini_max(self, player, depth=0):
         if player == "o":
             best = -10
         else:
             best = 10
         if self.complete():
-            if self.getWinner() == "x":
+            if self.get_winner() == "x":
                 return -10 + depth, None
-            elif self.getWinner() == "tie":
+            elif self.get_winner() == "tie":
                 return 0, None
-            elif self.getWinner() == "o":
+            elif self.get_winner() == "o":
                 return 10 - depth, None
-        for move in self.getAvailableMoves():
-            self.makeMove(move, player)
-            val, _ = self.minimax(self.getEnemyPlayer(player), depth+1)
+        for move in self.get_available_moves():
+            self.make_move(move, player)
+            val, _ = self.mini_max(self.get_enemy_player(player), depth + 1)
             print(val)
             if player == "o":
                 if val > best:
-                    best, bestMove = val, move
+                    best, best_move = val, move
                     if depth == 0:
-                        print("Currently best move would be: ", bestMove)
+                        print("Currently best move would be: ", best_move)
             else:
                 if val < best:
-                    best, bestMove = val, move
-            self.makeMove(move, ".")
-        return best, bestMove
+                    best, best_move = val, move
+            self.make_move(move, ".")
+        return best, best_move
 
-def gameStart():
+
+def game_start():
     game = TicTacToe()
-    game.createBoard()
-    game.makeMove(3, "x")
-    game.makeMove(4, "o")
-    game.makeMove(5, "x")
-    game.makeMove(6, "o")
-    game.makeMove(7, "x")
+    game.create_board()
+    game.make_move(3, "x")
+    game.make_move(4, "o")
+    game.make_move(5, "x")
+    game.make_move(6, "o")
+    game.make_move(7, "x")
     print("Initial moves done")
-    val, bestMove = game.minimax("o")
-    print('best move', bestMove)
+    val, best_move = game.mini_max("o")
+    print('best move', best_move + 1)
 
-gameStart()
+
+game_start()
